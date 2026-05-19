bab7a463c36b47e8d41e6fc7e22f539cc65dcf13
Alberto Olmos <albrtho94@gmail.com>
2026-05-19 17:39:08 -0600

    Ver, Editar & Eliminar - Autor

--- libros/urls.py
+++ libros/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AutorApiView
+from .views import AutorApiView, AutorDetailApiView
 
 
 urlpatterns = [
   path('autores/',AutorApiView.as_view(),name='listar_autores'),
+  path('autores/<int:pk>/', AutorDetailApiView.as_view(), name="detalle_autores"),
 ]
--- libros/views.py
+++ libros/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from .models import Autor
 from .serializer import AutorSerializer
@@ -26,6 +27,37 @@ class AutorApiView(APIView):
       return Response(autor.data, status = status.HTTP_201_CREATED)
     
     return Response(autor.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AutorDetailApiView(APIView):
+  def get_object(self, pk):
+      return get_object_or_404(Autor, pk=pk)
+  
+  #ver
+  def get(self, request, pk):
+    autor_obj = self.get_object(pk)
+    serializer_autor = AutorSerializer(autor_obj)
+    return Response(serializer_autor.data, status=status.HTTP_200_OK)
+  
+  #editar
+  def put(self, request, pk):
+    obj_autor = self.get_object(pk)
+    autor_serializer = AutorSerializer(obj_autor,data=request.data)
+
+    if autor_serializer.is_valid():
+      autor_serializer.save()
+      return Response(autor_serializer.data, status = status.HTTP_200_OK)
+
+    return Response(autor_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  #eliminar
+  def delete(self, request, pk):
+      obj_autor = self.get_object(pk)
+      nombre = obj_autor.nombre
+      obj_autor.delete()
+      return Response({'message':f"Autor {nombre} se ha eliminado."}, status=status.HTTP_200_OK)
+
+
+    
 
 
 
